dfe64a006bfd9471c2d271fa6afc6d1ca95e0934
NAVS <ronnel.navarro2020@gmail.com>
2024-08-09 10:02:07 +0800

    remove inc and config

--- inc/css-link.php
+++
@@ -1,4 +0,0 @@
-<?php include_once '.config/dbconnection.php' ?>
-<link rel="stylesheet" href="assets/css/custom.css">
-<link rel="icon" href="/favicon.ico" type="image/x-icon">
-<link rel="stylesheet" href="https://unpkg.com/boxicons/css/boxicons.min.css">
--- inc/js-link.php
+++
@@ -1,4 +0,0 @@
-<!-- Scripts -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="assets/js/skeleton.js"></script>
-<script src="assets/js/cart.js"></script>
